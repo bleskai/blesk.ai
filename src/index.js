935b3c89ac546a9ce366be51110cbfc9bf30dd5a
bleskai <vojtech.bien@gmail.com>
2026-01-29 01:18:11 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app'; // Tady musí být velké A a tečka s lomítkem na začátku
+import App from './App'; // Tady musí být velké A a tečka s lomítkem na začátku
 
 const root = ReactDOM.createRoot(
